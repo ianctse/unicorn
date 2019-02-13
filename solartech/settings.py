c042487dc9fa6074c99ffbe879337fa3830a2e88
Ian Tse <ian.c.tse@gmail.com>
2019-02-12 22:35:07 -0800

    Updated database parameters and installed apps in settings.py

--- solartech/settings.py
+++ solartech/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pvmodules.apps.PvmodulesConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'solartech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'datahub',
+        'USER': 'ianctse',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
