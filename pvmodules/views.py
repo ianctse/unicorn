0a9276005963f9baaca9a03dbdeb547805d395e4
Ian Tse <ian.c.tse@gmail.com>
2019-02-17 21:12:18 -0800

    Created new views for a number of data classes

--- pvmodules/views.py
+++ pvmodules/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from .models import Manufacturer, PvModule, PvModuleDatasheet
+
+class ManufacturerView(generic.DetailView):
+
+    model = Manufacturer
+    template_name = 'pvmodules/manufacturer.html'
+
+class PvModuleIndexView(generic.ListView):
+
+    template_name = 'pvmodules/pvmoduleindex.html'
+    context_object_name = 'pvmodule_models_list'
+
+    def get_queryset(self):
+        """Return all pvmodules."""
+        return PvModule.objects.order_by('manufacturer', 'model_name')[:25]
+
+class PvModuleView(generic.DetailView):
+
+    model = PvModule
+    template_name = 'pvmodules/pvmoduledetail.html'
+
+class PvModuleDatasheetView(generic.DetailView):
+
+    model = PvModuleDatasheet
+    template_name = 'pvmodules/pvmoduledatasheet.html'
